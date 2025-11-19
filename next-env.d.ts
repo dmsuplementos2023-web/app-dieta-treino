0310303edbb5b6afbcba3d10e592fd2cd933c28c
dmsuplementos2023-web <dmsuplementos2023@gmail.com>
2025-11-19 00:22:25 -0300

    Update next-env.d.ts

--- next-env.d.ts
+++ next-env.d.ts
@@ -3,3 +3,5 @@
 
 // NOTE: This file should not be edited
 // see https://nextjs.org/docs/app/api-reference/config/typescript for more information.
+https://bdfittdlexzowtwcgfgh.supabase.co
+eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImJkZml0dGRsZXh6b3d0d2NnZmdoIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTc2MjkzNzc2OSwiZXhwIjoyMDc4NTEzNzY5fQ.IJagmBKELFg-6fORxqYe-Kg48NEe8R87uJXxqIlMYtw
